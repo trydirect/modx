a7f0e09a4ef553199fa7d49f79c185598ec52512
vsilent <jabberroid@gmail.com>
2019-05-24 15:01:12 +0300

    MODX tests

---
+++ tests.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import docker
+import requests
+
+client = docker.from_env()
+
+# Testing Modx build
+
+time.sleep(20)  # we expect all containers are up and running in 20 secs
+
+for c in client.containers.list():
+    print("{}: {}" .format(c.name, c.status))
+    if 'running' not in c.status:
+        print(c.logs())
+
+# # NGINX
+nginx = client.containers.get('modx')
+nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
+assert nginx.status == 'running'
+print(nginx_cfg.output.decode())
+# assert 'server_name _;' in nginx_cfg.output.decode()
+# assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
+# assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
+assert 'HTTP/1.1" 500' not in nginx.logs()
+
+
+db = client.containers.get('modxdb')
+assert db.status == 'running'
+cnf = db.exec_run("/usr/sbin/mysqld --verbose  --help")
+print(cnf.output.decode())
+db_log = db.logs()
+assert "mysqld: ready for connections" in db_log.decode()
+
+# check redirect to web installer
+curl = nginx.exec_run("curl -i http://localhost")
+print(curl.output.decode())
+# assert 'Location: /setup/' in curl.output.decode()
+# @todo run modx unit test
+
+
+# PHP-FPM
+php = client.containers.get('modx')
+print(php.logs())
+assert php.status == 'running'
+php_conf = php.exec_run("php-fpm7.1 -t")
+print(php_conf.output.decode())
+php_proc = php.exec_run("sh -c 'ps aux |grep php-fpm'")
+print(php_proc.output.decode())
+assert 'configuration file /etc/php/7.1/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
+assert 'php-fpm: master process (/etc/php/7.1/fpm/php-fpm.conf)' in php_proc.output.decode()
+# assert 'fpm is running, pid' in php.logs()
--- v2_6_2/dockerfiles/README.md
+++
@@ -1,4 +0,0 @@
-When build new image
-
-docker commit -m "" modx optimum/cms:modx-2.6.2-s
-docker push optimum/cms:modx-2.6.2-s
