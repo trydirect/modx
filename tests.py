b240352bb4af132ef7a4d77e3f2ce752f63e42b7
vsilent <jabberroid@gmail.com>
2019-05-28 17:16:07 +0300

    cleanup, php.ini fix, tests.py updates

--- tests.py
+++ tests.py
@@ -22,9 +22,9 @@ nginx = client.containers.get('modx')
 nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
 assert nginx.status == 'running'
 print(nginx_cfg.output.decode())
-# assert 'server_name _;' in nginx_cfg.output.decode()
-# assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
-# assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
+assert 'server_name _;' in nginx_cfg.output.decode()
+assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
+assert "location = /.well-known/acme-challenge/" in nginx_cfg.output.decode()
 assert 'HTTP/1.1" 500' not in nginx.logs()
 
 
@@ -35,12 +35,6 @@ print(cnf.output.decode())
 db_log = db.logs()
 assert "mysqld: ready for connections" in db_log.decode()
 
-# check redirect to web installer
-curl = nginx.exec_run("curl -i http://localhost")
-print(curl.output.decode())
-# assert 'Location: /setup/' in curl.output.decode()
-# @todo run modx unit test
-
 
 # PHP-FPM
 php = client.containers.get('modx')
@@ -49,7 +43,13 @@ assert php.status == 'running'
 php_conf = php.exec_run("php-fpm7.1 -t")
 print(php_conf.output.decode())
 php_proc = php.exec_run("sh -c 'ps aux |grep php-fpm'")
-print(php_proc.output.decode())
+# print(php_proc.output.decode())
 assert 'configuration file /etc/php/7.1/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
 assert 'php-fpm: master process (/etc/php/7.1/fpm/php-fpm.conf)' in php_proc.output.decode()
-# assert 'fpm is running, pid' in php.logs()
+assert 'php-fpm: pool www' in php_proc.output.decode()
+assert 'modx' in php_proc.output.decode()
+
+# check redirect to web installer
+response = requests.get('http://localhost/setup')
+assert "Loading..." in response.text
+
